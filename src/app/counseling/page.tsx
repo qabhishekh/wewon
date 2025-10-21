25d8854f7c1680871e06decb8fb808c2984e5f55
abhishekh-westack <abhishekh.ojha@westack.ai>
2025-10-21 23:11:15 +0530

    Add Counseling page and Search component; update Navbar links for proper routing.

--- src/app/counseling/page.tsx
+++ src/app/counseling/page.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import SearchInput from "@/components/counseling/Search";
+import MainHeading from "@/components/sections/MainHeading";
+import React from "react";
 
-const page = () => {
+export default function page() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="container mx-auto px-4 sm:px-0">
+        <MainHeading
+          top="Personalized Career Counselling for"
+          bottom="Your Bright Future"
+        />
+      </div>
+      <div className="container mx-auto mt-10 px-4 sm:px-0">
+        <div className="mb-4 flex justify-between items-center">
+          <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-[var(--primary)]">
+            Search Counselling
+          </h2>
+          <SearchInput />
+        </div>
+      </div>
+      <div>
+        
+      </div>
+    </>
   );
 }
-
-export default page;
---
+++ src/components/counseling/Counslings.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Counslings() {
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4">
+        
+      </div>
+    </>
+  );
+}
