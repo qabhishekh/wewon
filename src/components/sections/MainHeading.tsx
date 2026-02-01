84974a783f93cdfe1ad642fcd1c79d5d8b009072
abhishekh-westack <abhishekh.ojha@westack.ai>
2026-02-02 00:21:57 +0530

    style: Remove `white-space: pre` styling from MainHeading component's top and bottom text spans.

--- src/components/sections/MainHeading.tsx
+++ src/components/sections/MainHeading.tsx
@@ -16,10 +16,10 @@ const MainHeading = ({
     <h1
       className={`text-2xl sm:text-3xl md:text-5xl font-bold text-[var(--primary)] leading-tight md:leading-tight pt-14 pb-2 text-${align} ${className}`}
     >
-      <span style={{ whiteSpace: "pre" }}>{top}</span>
+      <span>{top}</span>
       {bottom && (
         <div className="text-xl md:text-4xl lg:text-5xl my-2">
-          <span style={{ whiteSpace: "pre" }}>{bottom}</span>
+          <span>{bottom}</span>
         </div>
       )}
     </h1>
